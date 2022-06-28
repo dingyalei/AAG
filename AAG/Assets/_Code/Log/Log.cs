ca6c8f58e145ed37c91f9033ccf6b38ff735d017
wangxuankai <1073223357@qq.com>
2022-06-28 21:33:15 +0800

    增加LogDLL用于调用

---
+++ AAG/Assets/_Code/Log/Log.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+//namespace Log
+//{
+    public static class Log
+    {
+        public static void Error(string fmt, params object[] objects)
+        {
+            if (objects == null || objects.Length == 0)
+            {
+                Debug.LogError(fmt);
+            }
+            else
+            {
+                Debug.LogErrorFormat(fmt, objects);
+            }
+        }
+
+        public static void Warning(string fmt, params object[] objects)
+        {
+            if (objects == null || objects.Length == 0)
+            {
+                Debug.LogWarning(fmt);
+            }
+            else
+            {
+                Debug.LogWarningFormat(fmt, objects);
+            }
+        }
+
+        public static void Info(string fmt, params object[] objects)
+        {
+            if (objects == null || objects.Length == 0)
+            {
+                Debug.Log(fmt);
+            }
+            else
+            {
+                Debug.LogFormat(fmt, objects);
+            }
+        }
+
+        public static void Assert(bool condition, string fmt, params object[] objects)
+        {
+            if (condition)
+            {
+                return;
+            }
+            
+            Info(fmt, objects);
+        }
+    }
+//}
