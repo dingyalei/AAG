e90064187d7f08047da80ca42cc603a91432b0a3
wangxuankai <1073223357@qq.com>
2022-07-05 01:41:11 +0800

    状态机实现

---
+++ AAG/Assets/_Code/Library/IState/IState.cs
@@ -0,0 +1,18 @@
+﻿namespace Library
+{
+    public interface IState
+    {
+        void Enter();
+        void Exit();
+        void Update();
+    }
+
+    public interface ITState<T> : IState
+    {
+        T Subject
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ AAG/Assets/_Code/Library/IState/IStateMachine.cs
@@ -0,0 +1,33 @@
+﻿namespace Library
+{
+    public class IStateMachine<T> where T : IState
+    {
+        public T CurrentState
+        {
+            get;
+            protected set;
+        }
+
+        public T PreviousState
+        {
+            get;
+            protected set;
+        }
+
+        void ChangeState(T nextState)
+        {
+            if (nextState != null && !Equals(nextState, CurrentState))
+            {
+                PreviousState = CurrentState;
+                CurrentState = nextState;
+
+                if (PreviousState != null)
+                {
+                    PreviousState.Exit();
+                }
+                
+                CurrentState.Enter();
+            }
+        }
+    }
+}
