50740c357a742d43ded54e8a27d15451e07ff498
wangxuankai <1073223357@qq.com>
2022-08-08 01:46:08 +0800

    增加Component组件

---
+++ AAG/Assets/_Code/Library/Component/AAComponent.cs
@@ -0,0 +1,58 @@
+﻿namespace Library
+{
+    public abstract class AAComponent
+    {
+        //是否禁用改组件
+        private bool _enable = true;
+
+        public bool Enable
+        {
+            get => _enable && !_detached;
+            set => _enable = value;
+        }
+
+        //是否挂载在角色上
+        private bool _detached = true; 
+        
+        public bool Detached
+        {
+            get => _detached;
+            set => _detached = value;
+        }
+        
+        private object _ownerLogic;
+
+        public object OwnerLogic => _ownerLogic;
+
+        public virtual void AddComponent(object ownerLogic)
+        {
+            _ownerLogic = ownerLogic;
+            RegisterEvent();
+        }
+
+        public virtual void RemoveComponent()
+        {
+            UnRegisterEvent();
+            _ownerLogic = null;
+        }
+        
+        protected virtual void RegisterEvent() { }
+
+        protected virtual void UnRegisterEvent() { }
+
+        public abstract uint AAComponentID
+        {
+            get;
+        }
+
+        public virtual void Update(float fDeltaT)
+        {
+            
+        }
+
+        public virtual void LateUpdate(float fDeltaT)
+        {
+            
+        }
+    }
+}
